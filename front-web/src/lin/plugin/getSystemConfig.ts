d87c30c6056a4774104e32be7dd0991de6c07ebf
Pujing Cao <caopujing@717.com>
2024-08-20 18:02:44 +0800

    docs:增加getSYstemconifg注释

--- front-web/src/lin/plugin/getSystemConfig.ts
+++ front-web/src/lin/plugin/getSystemConfig.ts
@@ -54,14 +54,17 @@ const getsystemConfigFunc = async function () {
     // }
     // 获取全部菜单标题
     // const getMenus = function (menusTitleList, pathPrefix) {
+    // menusTitleList:字符串数组['首页...'], pathPrefix:系统前缀字符串'\fs'
     //     const menus = []
     //     for (const item of backRouter) {
+    // 这里是router文件夹导出的back-rouer，目前导出为格式化后的config文件夹back-stage文件夹的index文件
     //         if (isIncludesTitle(menusTitleList, item)) {
+    // isIncludesTitle 返回一个确定存在的title
     //             const oldName = typeof item.name === 'symbol' ? item.name.description : item.name
     //             const routeObj = {
     //                 ...item,
     //                 path: `${pathPrefix}${item.path}`,
-    //                 name: Symbol(`${pathPrefix}-${oldName}`)
+    //                 name: Symbol(`${pathPrefix}-${oldName}`) // 确保name一定是唯一的
     //             }
     //             menus.push(routeObj)
     //         }
@@ -73,23 +76,75 @@ const getsystemConfigFunc = async function () {
     // })
     // .then(response => response.json())
     // .then(res => {
+    // 返回值：res的data[]
+    //   "extra": {
+    //     "isHiddenSidebarLogo": "true"
+    //   },
+    //   "fullname": "事理图谱",
+    //   "id": 7,
+    //   "is_default": false,
+    //   "is_enable": true,
+    //   "menus": [
+    //     {
+    //       "children": [],
+    //       "label": "首页"
+    //     },
+    //     {
+    //       "children": [
+    //         {
+    //           "label": "用户"
+    //         },
+    //         {
+    //           "label": "角色"
+    //         }
+    //       ],
+    //       "label": "用户管理"
+    //     }
+    //   ],
+    //   "name": "事理图谱",
+    //   "permissions": [
+    //     {
+    //       "children": [
+    //         {
+    //           "id": 63,
+    //           "label": "创建事理图谱新闻",
+    //           "module": "事理图谱新闻"
+    //         },
+    //         {
+    //           "id": 64,
+    //           "label": "更新事理图谱新闻",
+    //           "module": "事理图谱新闻"
+    //         }
+    //       ],
+    //       "id": "事理图谱新闻",
+    //       "label": "事理图谱新闻"
+    //     }
+    //   ],
+    //   "route": "/sl"
+    // }
     //     if (res.code !== 0) {
     //         Vue.prototype.$message.error('系统配置项加载错误，请重试')
     //         return
     //     }
+    // 所有系统里启用路由的data项
     //     const systemList = res.data.filter(i => i.is_enable)
+
     //     setSystemConfig(systemList)
     //     let newBackStageConfig = []
     //     systemList.forEach(system => {
+    //     切换默认路由，向vue写入全局配置
     //         if (window.location.pathname.includes(system.route) || system.is_default) {
     //             setSystem(system.route.slice(1))
     //             Vue.prototype['$CONFIG'] = Utils.deepClone(system.extra)
     //             document.title = system.fullname
     //         }
-    //         // const menusTitleList = system.menus.map(i => i.label)
+
     //         const menusTitleList = getMenusList(system.menus)
+    // //system.menus: [{"children": [],"label": "首页"}]
+    // getMenusList计算过后（抽取所有menusLabel）：["首页","目标管理(金字塔目标管理)","遥感数据(金字塔遥感数据)","开源数据(金字塔开源数据)","态势要素抽取与关联","态势金字塔查询","典型突发事件的态势表达","用户","角色"]
     //         newBackStageConfig = newBackStageConfig.concat(updateBackStageConfig(stageConfig, system.route))
-    //         const menus = getMenus(menusTitleList, system.route)
+    //         // stageConfig：是导出的config->BackStageConfig->index后端路由
+    // const menus = getMenus(menusTitleList, system.route)
 
     //         router.addRoutes([{
     //             path: `${system.route}/admin/login`,
@@ -152,15 +207,18 @@ const getsystemConfigFunc = async function () {
     //     })
     //     setBackStageConfig(newBackStageConfig)
     // })
-    const componentMap = {
+    const componentMap: { [key: string]: string } = {
         '/main': 'MainView',
         '/pathEditor': 'PathEditorView',
         '/another': 'AnotherView', // 为新的组件添加映射
     };
     const portalRoutes: RouteRecordRaw[] = []
-    Array.isArray(FRONT_CONFIG['/transc']) && FRONT_CONFIG['/transc'].forEach((item:string) => {
+    Array.isArray(FRONT_CONFIG['/transc']) && FRONT_CONFIG['/transc'].forEach((item: any) => {
         const path = `${'/transc'}${item === '/main' ? '' : item}`
-        const componentName = isKey(item, componentMap) ? componentMap[item] : 'MainView'; // 使用映射获取组件名
+        let componentName = 'MainView'; // 使用映射获取组件名
+        if (item in componentMap) {
+            componentName = componentMap[item];
+        }
         const _component = Views[componentName as keyof typeof Views];
         portalRoutes.push({
             path,
@@ -209,10 +267,6 @@ const getsystemConfigFunc = async function () {
     //     }
     // ];
     //     map.map(i => (router.addRoute(i)))
-}
-// 类型守卫函数，检查item是否是componentMap的有效键
-function isKey<T>(key: string, obj: Record<string, T>): key is keyof typeof obj {
-    return key in obj;
 }
 export const getSystemConfigPlugin = async function () {
     console.log("初始化getSystemConfigPlugin");
---
+++ front-web/src/views/front-stage/main.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        现在你来到FrontMainView主页了
+    </div>
+</template>
+
+<script lang="ts" setup name="FrontMainView">
+</script>
+
+<style lang="scss" scoped></style>
